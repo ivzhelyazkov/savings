5e04f718d60a1893096da8481ee75fb9b8404947
Ivaylo Zhelyazkov <iv.zhelyazkov@gmail.com>
2022-04-11 23:41:55 +0300

    fixed more css in home

--- savings/main/admin.py
+++ savings/main/admin.py
@@ -5,25 +5,22 @@ from savings.main.models import IncomingCategory, Incoming, ExpenseCategory, Exp
 
 @admin.register(IncomingCategory)
 class IncomingCategoryAdmin(admin.ModelAdmin):
-    # inlines = (PetInlineAdmin,)
+    # inlines =
     list_display = ('name', 'color')
 
 
 @admin.register(Incoming)
 class IncomingAdmin(admin.ModelAdmin):
-    # inlines = (PetInlineAdmin,)
     list_display = ('amount', 'date', 'category', 'user')
 
 
 @admin.register(ExpenseCategory)
 class ExpenseCategoryAdmin(admin.ModelAdmin):
-    # inlines = (PetInlineAdmin,)
     list_display = ('name', 'color')
 
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    # inlines = (PetInlineAdmin,)
     list_display = ('amount', 'date', 'type', 'category', 'user')
 
 
